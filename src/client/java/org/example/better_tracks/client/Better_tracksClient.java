897e0a44ad03fc4499f23182fe698ae3a18f65a5
FreshIce <unsecure@nautics.cc>
2025-06-21 13:28:22 -0500

    sa

---
+++ src/client/java/org/example/better_tracks/client/Better_tracksClient.java
@@ -0,0 +1,10 @@
+package org.example.better_tracks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Better_tracksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/example/better_tracks/Better_tracks.java
@@ -0,0 +1,10 @@
+package org.example.better_tracks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Better_tracks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
